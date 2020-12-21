5f74e5d8e2284bce289b0e20392fb137e0da5afe
Dang Trung Hieu <dangtrunghieu2304@gmail.com>
2020-12-21 11:16:13 +0700

    add login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,46 +1,59 @@
-// import {Form,Button,Grid,Segment,Header as SemanticHeader} from 'semantic-ui-react';
-import React from 'react'
-import { Link} from 'react-router-dom';
-
-function pLogin() {
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import $ from 'jquery';
+function Login() {
+    useEffect(() => {
+        let $input = $('.c-form__input');
+        $input
+            .blur(function () {
+                if (!$(this).val()) {
+                    $(this).parent().removeClass('is-focus');
+                }
+            })
+            .focus(function () {
+                $(this).parent().addClass('is-focus');
+            });
+    }, []);
     return (
-                <>
-                {/* <Grid centered>
-                <Grid.Column style={{ maxWidth: 550, marginTop: 150 }}>
-                    <SemanticHeader>Login to your account</SemanticHeader>     
-                    <Segment>
-                    <Form>
-                        <Form.Field>
-                        <Form.Input
-
-                            name="username"
-                            placeholder="Username"
-                            label="Username"
-                        />
-                        </Form.Field>      
-                        <Form.Field>
-                        <Form.Input                        
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            label="Password"
-                        />
-                        </Form.Field>        
-                        <Button
-                        primary
-                        type="submit"
-                        >
-                        Login
-                        </Button>       
-                        <Segment >
-                            Need an account? <Link to="/auth/register"> Register</Link>
-                        </Segment>
-                    </Form>
-                    </Segment>
-                </Grid.Column>
-                </Grid> */}
-                </>
-    )
+        <>
+            <div className='login'>
+                <div className='c-overlayer'> </div>
+                <div className='login__content'>
+                    <div className='c-form'>
+                        <h3 className='c-form__title'>Login</h3>
+                        <form action=''>
+                            <div className='c-form__group'>
+                                <label className='c-form__label'>
+                                    Username
+                                </label>
+                                <input className='c-form__input' type='text' />
+                            </div>
+                            <div className='c-form__group'>
+                                <label className='c-form__label'>
+                                    Password
+                                </label>
+                                <input className='c-form__input' type='password' />
+                            </div>
+                            <div className='c-form__actions'>
+                                <button
+                                    className='c-btn c-btn--primary'
+                                    type='button'
+                                >
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                        <div className='c-form__below'>
+                            <Link to='/register'>Need help?</Link>
+                        </div>
+                        <div className='c-form__below'>
+                            <Link to='/register'>Need an account?</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default pLogin
+export default Login;
