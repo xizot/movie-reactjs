965f9ff4debad95e24194097caad4a2a61146cd9
Hieu dang <dangtrunghieu2304@gmail.com>
2021-01-01 18:21:00 +0700

    info finish

--- src/components/InfoUser.jsx
+++ src/components/InfoUser.jsx
@@ -4,81 +4,105 @@ function InfoUser() {
         <div className="infouser">
             <div className="c-overlayer"> </div>
             <div className="infouser__content">
-            <div className='c-form'>
-                <h3 className='c-form__title'>Personal Information</h3>
-                <form action=''>
-                    <div className='c-form__group'>
-                        <input
-                            className='c-form__input'
-                            type='text'
-                            required
-                        />
-                        <label className='c-form__label'>
-                            Username
-                        </label>
-                    </div>
-                    <div className='c-form__group'>
-                        <input
-                            className='c-form__input'
-                            type='email'
-                            required
-                        />
-                        <label className='c-form__label'>Email</label>
-                    </div>
-                    <div className='c-form__group'>
-                        <input
-                            className='c-form__input'
-                            type='text'
-                            required
-                        />
-                        <label className='c-form__label'>
-                            Display name
-                        </label>
-                    </div>
-                    <div className='c-form__group is-focus'>
-                        <input
-                            className='c-form__input_date'
-                            type='date'
-                            min='1920-01-01'
-                            max='2021-12-12'
-                            required
-                        />
-                        <label className='c-form__label'>
-                            Birthday
-                        </label>
-                    </div>
-                    <div className='c-form__group'>
-                        <input
-                            className='c-form__input'
-                            autoComplete='new-password'
-                            type='password'
-                            required
-                        />
-                        <label className='c-form__label'>
-                            Password
-                        </label>
-                    </div>
-
-                    <div className='c-form__group'>
-                        <input
-                            className='c-form__input'
-                            type='password'
-                            required
-                        />
-                        <label className='c-form__label'>
-                            Retype password
-                        </label>
-                    </div>
-                    <div className='c-form__actions'>
-                        <button
-                            className='c-btn c-btn--primary'
-                            type='button'
-                        >
-                            Register
-                        </button>
-                    </div>
-                </form>
-            </div>
+                <div className="content_form">
+                    <div className="title_form">
+                        <div className="title_form__left">
+                            <h3 className='content_form__title'>Your Profile</h3>
+                            <h5 className='content_form__son'>Edit your name, avatar, etc..</h5>
+                        </div>
+                        <div className='title_form__right'>
+                            <button
+                                className="x-btn c-btn"
+                                type='button'>
+                                X DISCARD CHANGES
+                            </button> 
+                        </div>             
+                    </div>     
+                    <div className="index_form">
+                        <div className="infouser__left">
+                            <label htmlFor="upload-avatar">
+                                <img
+                                    src={`${process.env.PUBLIC_URL}/assets/img/avata.jpg`}
+                                    alt=''
+                                />
+                            </label>
+                            <input type="file" name="" id="upload-avatar" style={{display:'none'}}/>
+                        </div>
+                        <div className="infouser__right">
+                            <div className='c-form'>                            
+                                <form action=''>
+                                    <div className='c-form__group'>
+                                        <input
+                                            className='c-form__input'
+                                            type='text'
+                                            required
+                                        />
+                                        <label className='c-form__label'>
+                                            USERNAME
+                                        </label>
+                                    </div>
+                                    <div className='c-form__group'>
+                                        <input
+                                            className='c-form__input'
+                                            type='text'
+                                            required
+                                        />
+                                        <label className='c-form__label'>
+                                            NAME
+                                        </label>
+                                    </div>
+                                    <div className='c-form__group'>
+                                        <input
+                                            className='c-form__input'
+                                            type='text'
+                                            required
+                                        />
+                                        <label className='c-form__label'>EMAIL</label>
+                                    </div>
+                                
+                                    <div className='c-form__group is-focus'>
+                                        <input
+                                            className='c-form__input_date'
+                                            type='date'
+                                            min='1920-01-01'
+                                            max='2021-12-12'
+                                            required
+                                        />
+                                        <label className='c-form__label'>
+                                            BIRTHDAY
+                                        </label>
+                                    </div>
+                                    <div className='c-form__group'>
+                                        <input
+                                            className='c-form__input'
+                                            autoComplete='new-password'
+                                            type='password'
+                                            required
+                                        />
+                                        <label className='c-form__label'>
+                                            PASSWORD
+                                        </label>
+                                    </div>
+                                    <div className='c-form__group'>
+                                        <input
+                                            className='c-form__input'
+                                            type='password'
+                                            required
+                                        />
+                                        <label className='c-form__label'>
+                                            RE-PASSWORD
+                                        </label>
+                                    </div>
+                                    <button
+                                        className='c-btn c-btn'
+                                        type='button'>
+                                        SAVE CHANGES
+                                    </button> 
+                                </form>
+                            </div>
+                        </div>
+                    </div>                           
+                </div>            
             </div>
         </div>
     );
